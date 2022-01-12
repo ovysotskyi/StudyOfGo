b55743327929ef638e767d81eb25e6764af61ecd
OVysotskyi <oleksii.vysotskyi@gmail.com>
2022-01-12 16:38:05 +0200

    added new tasks

--- 2_Functions_Structures_etc/2.5_Strings/task1/main.go
+++ 2_Functions_Structures_etc/2.5_Strings/task1/main.go
@@ -13,6 +13,7 @@ func main() {
 	str := []rune(text)
 
 	// -3 because len - 1 - rune(\n); rune(\n) = 2 bite
+	fmt.Println(utf8.RuneCountInString(text))
 	if unicode.IsUpper(str[0]) && str[utf8.RuneCountInString(text)-3] == '.' {
 		fmt.Print("Right")
 	} else {
--- 2_Functions_Structures_etc/2.5_Strings/task2/main.go
+++ 2_Functions_Structures_etc/2.5_Strings/task2/main.go
@@ -1,14 +1,38 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+	"unicode/utf8"
+)
+
 func main() {
-	// text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	text, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+	text = strings.Replace(text, " ", "", -1)
 
-	// text = strings.Replace(text, " ", "", -1)
+	str := []rune(text)
+	var rihtRunes []rune
 
-	// str := []rune(text)
+	//-2 because len - rune(\n); rune(\n) = 2 bite
+	strSize := (utf8.RuneCountInString(text) - 2)
 
-	// fmt.Println(text)
+	if strSize%2 == 0 {
+		right := str[strSize/2 : strSize]
+		for i := len(right) - 1; i >= 0; i-- {
+			rihtRunes = append(rihtRunes, right[i])
+		}
+	} else {
+		right := str[strSize/2+1 : strSize]
+		for i := len(right) - 1; i >= 0; i-- {
+			rihtRunes = append(rihtRunes, right[i])
+		}
+	}
 
-	// -3 because len - 1 - rune(\n); rune(\n) = 2 bite
-
+	if string(str[:strSize/2]) == string(rihtRunes) {
+		fmt.Println("Палиндром")
+	} else {
+		fmt.Println("Нет")
+	}
 }
