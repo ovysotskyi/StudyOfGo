8b0fb5aa82f5bf7e61c69975e3a0fceb73a8aa58
OVysotskyi <oleksii.vysotskyi@gmail.com>
2022-01-14 09:00:22 +0200

    fixed alg

--- 1_Introduction/1.12_ArraysAndSlices/task3/main.go
+++ 1_Introduction/1.12_ArraysAndSlices/task3/main.go
@@ -1,19 +1,18 @@
 package main
+
 import "fmt"
 
-func main()  {
+func main() {
 	array := [5]int{}
 	var a int
-	for i:=0; i < 5; i++{
+	for i := 0; i < 5; i++ {
 		fmt.Scan(&a)
 		array[i] = a
 	}
-    // здесь ваш код
+	// здесь ваш код
 	var aMax int
 	for _, value := range array {
-		if value > aMax {
-			aMax = value
-		} else if aMax == 0 && value < 0 {
+		if value > aMax || (aMax == 0 && value < 0) {
 			aMax = value
 		}
 	}
