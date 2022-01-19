a518de83574d5a3a994fe47baff9d78707a94388
OVysotskyi <oleksii.vysotskyi@gmail.com>
2022-01-19 12:36:39 +0200

    added new alg

--- 3_Map_interfaces_files_etc/3.4_Interfaces/task2/main.go
+++ 3_Map_interfaces_files_etc/3.4_Interfaces/task2/main.go
@@ -2,38 +2,45 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 type Battery struct {
-	chargeSize string
+	charge string
 }
 
 func (b Battery) String() string {
-	size := "["
-
-	units := 0
-	for _, value := range b.chargeSize {
-		if value == rune('1') {
-			units++
-		}
-	}
-
-	for i := 0; i < 10-units; i++ {
-		size += " "
-	}
-	for i := 0; i < units; i++ {
-		size += "X"
-	}
-
-	size += "]"
-
-	return size
+	ans := strings.Repeat("X", strings.Count(b.charge, "1"))
+	return fmt.Sprintf("[%10v]", ans)
 }
+
+// or
+// func (b Battery) String() string {
+// 	size := "["
+
+// 	units := 0
+// 	for _, value := range b.charge {
+// 		if value == rune('1') {
+// 			units++
+// 		}
+// 	}
+
+// 	for i := 0; i < 10-units; i++ {
+// 		size += " "
+// 	}
+// 	for i := 0; i < units; i++ {
+// 		size += "X"
+// 	}
+
+// 	size += "]"
+
+// 	return size
+// }
 
 func main() {
 	var batteryForTest Battery
 
-	fmt.Scan(&batteryForTest.chargeSize)
+	fmt.Scan(&batteryForTest.charge)
 
 	fmt.Print(batteryForTest)
 }
