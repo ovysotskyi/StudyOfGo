ae831f5d420de548fd7dbe29af21af3072711289
OVysotskyi <oleksii.vysotskyi@gmail.com>
2022-01-20 16:15:55 +0200

    fix alg

--- 1_Introduction/1.10_Cycles/task8/main.go
+++ 1_Introduction/1.10_Cycles/task8/main.go
@@ -7,19 +7,17 @@ func main() {
 	fmt.Scan(&num1, &num2)
 
 	for num1 != 0 {
-		if num1 >= 0 && num1 < 10000 {
-			num1_temp := num1 % 10
-			num1 = num1 / 10
+		num1_temp := num1 % 10
+		num1 = num1 / 10
 
-			num2_new := num2
-			for num2_new != 0 {
-				num2_temp := num2_new % 10
-				if num1_temp == num2_temp {
-					num3 = (num3 * 10) + num1_temp
-				}
-
-				num2_new = num2_new / 10
+		num2_new := num2
+		for num2_new != 0 {
+			num2_temp := num2_new % 10
+			if num1_temp == num2_temp {
+				num3 = (num3 * 10) + num1_temp
 			}
+
+			num2_new = num2_new / 10
 		}
 	}
 
