5a2de8ed37ed7f5be9eb3213c3f00008caeea2bc
OVysotskyi <oleksii.vysotskyi@gmail.com>
2022-01-20 16:56:39 +0200

    --

--- 1_Introduction/1.10_Cycles/task8/main.go
+++ 1_Introduction/1.10_Cycles/task8/main.go
@@ -6,23 +6,35 @@ func main() {
 	var num1, num2, num3 int
 	fmt.Scan(&num1, &num2)
 
-	for num1 != 0 {
-		num1_temp := num1 % 10
-		num1 = num1 / 10
+	for i := 0; i < 4; i++ {
+		if num1 >= 0 && num1 < 10000 {
+			num1_temp := num1 % 10
+			num1 = num1 / 10
 
-		num2_new := num2
-		for num2_new != 0 {
-			num2_temp := num2_new % 10
-			if num1_temp == num2_temp {
-				num3 = (num3 * 10) + num1_temp
+			num2_new := num2
+			for j := 0; j < 4; j++ {
+				num2_temp := num2_new % 10
+				if num1_temp == num2_temp {
+					num3 = (num3 * 10) + num1_temp
+				}
+
+				num2_new = num2_new / 10
+				if num2_new < 1 {
+					break
+				}
 			}
 
-			num2_new = num2_new / 10
+			if num1 < 1 {
+				break
+			}
 		}
 	}
 
-	for num3 != 0 {
+	for i := 0; i < 4; i++ {
 		fmt.Print(num3%10, " ")
 		num3 = num3 / 10
+		if num3 < 1 {
+			break
+		}
 	}
 }
